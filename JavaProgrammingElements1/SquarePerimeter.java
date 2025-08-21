99444a0d0ecba967e3c57e9d781ad8bb94b16d9f
Shraddha s <shraddha09singh06@gmail.com>
2025-08-21 22:36:01 +0530

    JavaProgrammingElements

---
+++ JavaProgrammingElements1/SquarePerimeter.java
@@ -0,0 +1,16 @@
+package labnew.JavaProgrammingElements.JavaProgrammingElements1;
+
+import java.util.Scanner;
+
+public class SquarePerimeter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the perimeter of the square: ");
+        int perimeter = sc.nextInt();
+
+        int side = perimeter / 4;
+
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+
+}}
