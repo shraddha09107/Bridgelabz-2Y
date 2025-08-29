a8a0e072f89f42385b6f057e52fdf7607d659a3d
shraddha09107 <shraddha09singh06@gmail.com>
2025-08-29 16:05:51 +0530

    Add files via upload

---
+++ JavaControlFlow1/PosNegZero.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class PosNegZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (number > 0) System.out.println("Positive");
+        else if (number < 0) System.out.println("Negative");
+        else System.out.println("Zero");
+    }
+}
