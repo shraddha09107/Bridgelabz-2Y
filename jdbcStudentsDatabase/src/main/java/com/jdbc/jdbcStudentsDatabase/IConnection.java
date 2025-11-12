5bbbb78eb55df534608c6432f6f142121101cf87
Shraddha singh <shraddha09singh06@gmail.com>
2025-11-12 21:16:04 +0530

    Delete jdbcStudentsDatabase/src/main/java/com/jdbc/jdbcStudentsDatabase directory

--- jdbcStudentsDatabase/src/main/java/com/jdbc/jdbcStudentsDatabase/IConnection.java
+++
@@ -1,9 +0,0 @@
-package com.jdbc.jdbcStudentsDatabase;
-
-import java.sql.Connection;
-
-public interface IConnection {
-	Connection getConnection();
-
-}
-
--- jdbcStudentsDatabase/src/main/java/com/jdbc/jdbcStudentsDatabase/IMenu.java
+++
@@ -1,5 +0,0 @@
-package com.jdbc.jdbcStudentsDatabase;
-
-public interface IMenu {
-	void showMenu();
-}
